81b7ea2c445e0b4802e250e1ccd6086cffa00fd9
Shohei <yukawa.shouhei@gmail.com>
2026-05-23 12:17:14 -0700

    Initial Commit

---
+++ Squat Sensei/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Squat Sensei
+//
+//  Created by 湯川昇平 on 2026/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Squat Sensei/Squat_SenseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Squat_SenseiApp.swift
+//  Squat Sensei
+//
+//  Created by 湯川昇平 on 2026/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Squat_SenseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
